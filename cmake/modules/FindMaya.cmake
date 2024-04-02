2b4a075c35ee58c39c3c28e8f67402e3e8799ca5
Andy Phillips <andy-phillips@users.noreply.github.com>
2024-04-02 16:06:20 +1300

    Documentation, consistent variable naming, more functions to reduce scope of temporary variables.

--- cmake/modules/FindMaya.cmake
+++ cmake/modules/FindMaya.cmake
@@ -1,82 +1,150 @@
-if (NOT DEFINED ENV{MAYA_LOCATION}
-    AND NOT DEFINED MAYA_LOCATION
-    AND NOT DEFINED MAYA_DEVKIT_LOCATION)
-    message(
-        "Please set the MAYA_LOCATION variable to the Maya installation path. "
-        "Alternatively set the CMake variable MAYA_DEVKIT_LOCATION to the Maya devkit path."
-    )
-endif()
+#[=============================================================================[.rst:
+FindMaya
+--------
 
-find_path(Maya_ROOT_DIR
+Finds the Maya SDK (devkit) libraries and Maya specific versions of 3rd pary libraries.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``Maya::Foundation``
+    The foundation library
+
+``Maya::Maya``
+    The OpenMaya library
+
+``Maya::Anim``
+    The OpenMayaAnim library
+
+``Maya::FX``
+    The OpenMayaFX library
+
+``Maya::Render``
+    The OpenMayaRender library
+
+``Maya::UI``
+    The OpenMayaUI library
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+``Maya_FOUND``
+    True if the system has the Maya SDK libraries.
+
+``Maya_VERSION_MAJOR``
+  The major version of the Maya API.
+
+``Maya_VERSION_MINOR``
+  The minor version of the Maya API.
+
+``Maya_VERSION``
+  Full version in the ``X.Y`` format.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+The following cache variables may also be set:
+
+``Maya_SDK_ROOT_DIR``
+    Path to the Maya SDK (defined if not set).
+
+#]=============================================================================]
+
+find_path(Maya_SDK_ROOT_DIR
     "include/maya/MFn.h"
     PATHS
-        "${MAYA_LOCATION}"
-        "$ENV{MAYA_LOCATION}"
         "${MAYA_DEVKIT_LOCATION}"
+        "${DEVKIT_LOCATION}"
+        "$ENV{MAYA_DEVKIT_LOCATION}"
     PATH_SUFFIXES
         "devkit"
 )
 
-find_path(Maya_LIBRARY_DIRS
+macro(maya_set_failure_message NAME_VAR PATH_VAR)
+    if(DEFINED ${NAME_VAR} AND NOT EXISTS "${PATH_VAR}")
+        set(Maya_FAILURE_MESSAGE "${NAME_VAR} is defined but the path does not exist: ")
+        string(CONCAT Maya_FAILURE_MESSAGE ${Maya_FAILURE_MESSAGE} "${PATH_VAR}")
+    else()
+        set(Maya_FAILURE_MESSAGE "${NAME_VAR} is not defined")
+    endif()
+endmacro()
+
+if(NOT Maya_SDK_ROOT_DIR)
+    maya_set_failure_message(MAYA_DEVKIT_LOCATION "${MAYA_DEVKIT_LOCATION}")
+endif()
+
+find_path(Maya_LIBRARY_DIR
     "libOpenMaya.dylib"
     "OpenMaya.lib"
     PATHS
-        "${Maya_ROOT_DIR}"
+        "${Maya_SDK_ROOT_DIR}"
     PATH_SUFFIXES
         "lib"
     NO_CACHE
 )
 
-find_path(MAYA_INCLUDE_DIR
+find_path(Maya_INCLUDE_DIR
     "maya/MFn.h"
     PATHS
-        "${Maya_ROOT_DIR}"
+        "${Maya_SDK_ROOT_DIR}"
     PATH_SUFFIXES
         "include"
     NO_CACHE
 )
 
-function(strip_version_leading_zeros VERSION_STRING VERSION_VARIABLE)
-    string(REGEX REPLACE "(^0+)" "" VERSION_STRING ${VERSION_STRING})
-    string(COMPARE EQUAL "${VERSION_STRING}" "" VERSION_IS_ZERO)
+function(maya_extract_version_from_file FILE_PATH)
+    file(STRINGS "${FILE_PATH}" VERSION_DEFINE REGEX "#define MAYA_API_VERSION.*$")
+    string(REGEX MATCHALL "[0-9]+" API_VERSION "${VERSION_DEFINE}")
+
+    string(SUBSTRING ${API_VERSION} "0" "4" Maya_VERSION_MAJOR)
+    set(Maya_VERSION_MAJOR ${Maya_VERSION_MAJOR} PARENT_SCOPE)
+
+    string(SUBSTRING ${API_VERSION} "4" "2" Maya_VERSION_MINOR)
+    string(REGEX REPLACE "(^0+)" "" Maya_VERSION_MINOR ${Maya_VERSION_MINOR})
+    string(COMPARE EQUAL ${Maya_VERSION_MINOR} "" VERSION_IS_ZERO)
 
     if(VERSION_IS_ZERO)
-        set(${VERSION_VARIABLE} "0" PARENT_SCOPE)
+        set(Maya_VERSION_MINOR 0 PARENT_SCOPE)
     else()
-        set(${VERSION_VARIABLE} ${VERSION_STRING} PARENT_SCOPE)
+        set(Maya_VERSION_MINOR ${Maya_VERSION_MINOR} PARENT_SCOPE)
     endif()
+
+    set(Maya_VERSION "${Maya_VERSION_MAJOR}.${Maya_VERSION_MINOR}" PARENT_SCOPE)
 endfunction()
 
-if(MAYA_INCLUDE_DIR)
-    list(APPEND Maya_INCLUDE_DIRS ${MAYA_INCLUDE_DIR})
-
-    if(EXISTS "${MAYA_INCLUDE_DIR}/maya/MTypes.h")
-        file(STRINGS ${MAYA_INCLUDE_DIR}/maya/MTypes.h MAYA_VERSION_DEFINE REGEX "#define MAYA_API_VERSION.*$")
-        string(REGEX MATCHALL "[0-9]+" MAYA_API_VERSION ${MAYA_VERSION_DEFINE})
-    endif()
-
-    string(SUBSTRING ${MAYA_API_VERSION} "0" "4" Maya_VERSION_MAJOR)
-
-    string(SUBSTRING ${MAYA_API_VERSION} "4" "2" Maya_VERSION_MINOR)
-    strip_version_leading_zeros(${Maya_VERSION_MINOR} Maya_VERSION_MINOR)
-
-    string(SUBSTRING ${MAYA_API_VERSION} "6" "-1" Maya_VERSION_PATCH)
-    strip_version_leading_zeros(${Maya_VERSION_PATCH} Maya_VERSION_PATCH)
-
-    set(Maya_VERSION "${Maya_VERSION_MAJOR}.${Maya_VERSION_MINOR}.${Maya_VERSION_PATCH}")
+if(Maya_INCLUDE_DIR AND EXISTS "${Maya_INCLUDE_DIR}/maya/MTypes.h")
+    maya_extract_version_from_file("${Maya_INCLUDE_DIR}/maya/MTypes.h")
 endif()
 
-find_program(Maya_EXECUTABLE maya
-    PATHS
-        "${MAYA_LOCATION}"
-        "$ENV{MAYA_LOCATION}"
-    PATH_SUFFIXES
-        bin/
-        Maya.app/Contents/bin/
-    DOC "Maya's executable path"
-)
+function(maya_find_executable)
+    if(APPLE)
+        set(DEFAULT_INSTALL_DIR "/Applications/Autodesk")
+    elseif(UNIX)
+        set(DEFAULT_INSTALL_DIR "/usr/autodesk")
+    elseif(WIN32)
+        set(DEFAULT_INSTALL_DIR "C:\\Program Files\\Autodesk")
+    endif()
 
-set(MAYA_COMPONENTS
+    find_program(Maya_EXECUTABLE
+        "maya"
+        PATHS
+            "${MAYA_LOCATION}"
+            "$ENV{MAYA_LOCATION}"
+            "${DEFAULT_INSTALL_DIR}"
+        PATH_SUFFIXES
+            "bin"
+            "maya${Maya_VERSION_MAJOR}/Maya.app/Contents/bin"
+        NO_CACHE
+    )
+
+    set(Maya_EXECUTABLE "${Maya_EXECUTABLE}" PARENT_SCOPE)
+endfunction()
+
+maya_find_executable()
+
+set(Maya_COMPONENTS
     Foundation:Foundation
     Maya:OpenMaya
     Anim:OpenMayaAnim
@@ -86,7 +154,7 @@ set(MAYA_COMPONENTS
     CLEW:clew
 )
 
-macro(_pair_to_key_value PAIR KEY VALUE)
+macro(maya_pair_to_key_value PAIR KEY VALUE)
     if(${PAIR} MATCHES "^([^:]+):(.*)$")
         set(${KEY} ${CMAKE_MATCH_1})
         set(${VALUE} ${CMAKE_MATCH_2})
@@ -95,95 +163,95 @@ macro(_pair_to_key_value PAIR KEY VALUE)
     endif()
 endmacro()
 
-foreach(COMPONENT_PAIR ${MAYA_COMPONENTS})
-    _pair_to_key_value(${COMPONENT_PAIR} _COMPONENT_NAME _LIB_NAME)
+foreach(COMPONENT_PAIR ${Maya_COMPONENTS})
+    maya_pair_to_key_value(${COMPONENT_PAIR} Maya_COMPONENT_NAME Maya_LIB_NAME)
 
     find_library(
-        MAYA_${_LIB_NAME}_LIBRARY ${_LIB_NAME}
+        Maya_${Maya_LIB_NAME}_LIBRARY ${Maya_LIB_NAME}
         PATHS
-            "${Maya_LIBRARY_DIRS}"
+            "${Maya_LIBRARY_DIR}"
         NO_CACHE
         # NO_CMAKE_SYSTEM_PATH needed to avoid conflicts between Maya's foundation library and
         # OSX's foundation framework.
         NO_CMAKE_SYSTEM_PATH
     )
 
-    if(MAYA_${_LIB_NAME}_LIBRARY)
-        set(Maya_${_COMPONENT_NAME}_FOUND TRUE)
+    if(Maya_${Maya_LIB_NAME}_LIBRARY)
+        set(Maya_${Maya_COMPONENT_NAME}_FOUND TRUE)
     endif()
 endforeach()
 
-find_library(_MAYA_TBB_LIBRARY_DEBUG tbb_debug PATHS "${Maya_LIBRARY_DIRS}" NO_CACHE)
-find_library(_MAYA_TBB_LIBRARY_RELEASE tbb PATHS "${Maya_LIBRARY_DIRS}" NO_CACHE)
-find_path(_MAYA_TBB_INCLUDE_DIR tbb/tbb.h PATHS "${MAYA_INCLUDE_DIR}" NO_CACHE)
+find_library(Maya_TBB_LIBRARY_DEBUG "tbb_debug" PATHS "${Maya_LIBRARY_DIR}" NO_CACHE)
+find_library(Maya_TBB_LIBRARY_RELEASE "tbb" PATHS "${Maya_LIBRARY_DIR}" NO_CACHE)
+find_path(Maya_TBB_INCLUDE_DIR "tbb/tbb.h" PATHS "${Maya_INCLUDE_DIR}" NO_CACHE)
 
-if(_MAYA_TBB_LIBRARY_DEBUG
-   OR _MAYA_TBB_LIBRARY_RELEASE
-   AND _MAYA_TBB_INCLUDE_DIR
+if(Maya_TBB_LIBRARY_DEBUG
+   OR Maya_TBB_LIBRARY_RELEASE
+   AND Maya_TBB_INCLUDE_DIR
 )
-    set(_TBB_TARGET_NAME Maya::TBB)
+    set(Maya_TBB_TARGET_NAME Maya::TBB)
 
     set(Maya_TBB_FOUND TRUE)
 
-    if(NOT TARGET ${_TBB_TARGET_NAME})
-        add_library(${_TBB_TARGET_NAME} UNKNOWN IMPORTED)
+    if(NOT TARGET ${Maya_TBB_TARGET_NAME})
+        add_library(${Maya_TBB_TARGET_NAME} UNKNOWN IMPORTED)
     endif()
 
-    set_target_properties(${_TBB_TARGET_NAME}
+    set_target_properties(${Maya_TBB_TARGET_NAME}
         PROPERTIES
-            IMPORTED_LOCATION "${_MAYA_TBB_LIBRARY_RELEASE}"
-            INTERFACE_INCLUDE_DIRECTORIES "${_MAYA_TBB_INCLUDE_DIR}"
+            IMPORTED_LOCATION "${Maya_TBB_LIBRARY_RELEASE}"
+            INTERFACE_INCLUDE_DIRECTORIES "${Maya_TBB_INCLUDE_DIR}"
     )
 
-    if(_MAYA_TBB_LIBRARY_DEBUG)
-        set_property(TARGET ${_TBB_TARGET_NAME}
+    if(Maya_TBB_LIBRARY_DEBUG)
+        set_property(TARGET ${Maya_TBB_TARGET_NAME}
             APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG
         )
-        set_target_properties(${_TBB_TARGET_NAME}
+        set_target_properties(${Maya_TBB_TARGET_NAME}
             PROPERTIES
-                IMPORTED_LOCATION_DEBUG "${_MAYA_TBB_LIBRARY_DEBUG}")
+                IMPORTED_LOCATION_DEBUG "${Maya_TBB_LIBRARY_DEBUG}")
     endif()
 
-    unset(_TBB_TARGET_NAME)
+    unset(Maya_TBB_TARGET_NAME)
 endif()
 
-unset(_MAYA_TBB_LIBRARY_DEBUG)
-unset(_MAYA_TBB_LIBRARY_RELEASE)
-unset(_MAYA_TBB_INCLUDE_DIR)
+unset(Maya_TBB_LIBRARY_DEBUG)
+unset(Maya_TBB_LIBRARY_RELEASE)
+unset(Maya_TBB_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
 
+# NOTE: If the package was found, it will print the contents of the first
+# required variable to indicate where it was found.
 find_package_handle_standard_args(
     Maya
     VERSION_VAR Maya_VERSION
+    REQUIRED_VARS
+        Maya_SDK_ROOT_DIR
+        Maya_EXECUTABLE
     HANDLE_VERSION_RANGE
     HANDLE_COMPONENTS
+    REASON_FAILURE_MESSAGE "${Maya_FAILURE_MESSAGE}"
 )
+unset(Maya_FAILURE_MESSAGE)
 
-foreach(COMPONENT_PAIR ${MAYA_COMPONENTS})
-    _pair_to_key_value(${COMPONENT_PAIR} _COMPONENT_NAME _LIB_NAME)
+foreach(COMPONENT_PAIR ${Maya_COMPONENTS})
+    maya_pair_to_key_value(${COMPONENT_PAIR} Maya_COMPONENT_NAME Maya_LIB_NAME)
 
-    if(MAYA_${_LIB_NAME}_LIBRARY)
-        set(TARGET_NAME Maya::${_COMPONENT_NAME})
+    if(Maya_${Maya_LIB_NAME}_LIBRARY)
+        set(Maya_TARGET_NAME Maya::${Maya_COMPONENT_NAME})
 
-        if(NOT TARGET ${TARGET_NAME})
-            add_library(${TARGET_NAME} UNKNOWN IMPORTED)
-            set_target_properties(${TARGET_NAME}
+        if(NOT TARGET ${Maya_TARGET_NAME})
+            add_library(${Maya_TARGET_NAME} UNKNOWN IMPORTED)
+            set_target_properties(${Maya_TARGET_NAME}
                 PROPERTIES
-                    IMPORTED_LOCATION "${MAYA_${_LIB_NAME}_LIBRARY}"
-                    INTERFACE_INCLUDE_DIRECTORIES "${MAYA_INCLUDE_DIR}"
+                    IMPORTED_LOCATION "${Maya_${Maya_LIB_NAME}_LIBRARY}"
+                    INTERFACE_INCLUDE_DIRECTORIES "${Maya_INCLUDE_DIR}"
             )
-            list(APPEND Maya_LIBRARIES ${TARGET_NAME})
         endif()
     endif()
 endforeach()
 
-mark_as_advanced(
-    Maya_INCLUDE_DIRS
-    Maya_LIBRARIES
-)
-
-unset(MAYA_INCLUDE_DIR)
-unset(MAYA_LIBRARIES_DIR)
-unset(MAYA_COMPONENTS)
-unset(_COMPONENT_NAME)
+unset(Maya_INCLUDE_DIR)
+unset(Maya_LIBRARY_DIR)
+unset(Maya_COMPONENTS)
